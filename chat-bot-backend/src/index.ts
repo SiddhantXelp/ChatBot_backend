799f24bfcd7d4e9e9f77651a74f14fdf6f84ee07
SiddhantXelp <siddhant.kumar@xelpmoc.in>
2023-02-16 19:29:13 +0530

    Swagger_Connection

--- chat-bot-backend/src/index.ts
+++ chat-bot-backend/src/index.ts
@@ -5,6 +5,9 @@ import connects from "./config/db";
 import responseRouter from "./routes/response.routes";
 import widgetRouter from "./routes/widget.route";
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger");
+
 connects();
 
 const app = express();
@@ -13,6 +16,55 @@ app.use(bodyParser.json());
 // app.use(express.urlencoded({ extended: true }));
 
 const PORT = 4011;
+
+// var options = {
+//   explorer: true,
+// };
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Fitness Gram API",
+      version: "1.0.0",
+      description: "Fitness Gram API",
+    },
+    swaggerOptions: {
+      // basicAuth: {
+      //   name: 'authorization',
+      //   schema: {
+      //     type: 'basic',
+      //     in: 'header'
+      //   },
+      //   value:  'Basic <user>'
+      // }
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+        },
+      },
+    },
+
+    security: [{ bearerAuth: [] }],
+    securityDefinitions: {
+      bearerAuth: {
+        type: "apiKey",
+        name: "authorization",
+        in: "header",
+        description: "Authentication token",
+      },
+    },
+  },
+  apis: ["./swagger.ts"],
+};
+// const specs = swaggerJsDoc(options);
+// const customCss = fs.readFileSync((process.cwd() + "/src/swagger.css"), 'utf8');
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs, { customCss }));
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(widgetRouter);
 app.use(responseRouter);
